5606c418fcb961f13791a06dca809354195e1204
AlejandraCP <alejandracp710@gmail.com>
2018-01-09 02:06:19 -0500

    Agregando vista perfil

---
+++ public/js/data.js
@@ -0,0 +1,202 @@
+var data = [{
+  'name': '',
+  'type': 'vegetariana',
+  'district': 'Miraflores',
+  'address': 'Av.La Paz 838',
+  'price': '$$',
+  'phonenumber': '01 4772521',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 9:00 pm',
+  'image': 'assets/images/veg1.jpg',
+  'restaurant': 'assets/images/jardin-de-jazmin.jpg'
+},
+{
+  'name': 'xauxa',
+  'type': 'vegetariana',
+  'district': 'La Victoria',
+  'address': 'Av.Mexico 981',
+  'price': '$',
+  'phonenumber': '01 4772581',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 11:00 pm',
+  'image': 'assets/images/veg2.jpg',
+  'restaurant': 'assets/images/xauxa.jpg'
+},
+{
+  'name': 'madre natura',
+  'type': 'vegetariana',
+  'district': 'Miraflores',
+  'address': ' Jr.Chiclayo 815',
+  'price': '$$',
+  'phonenumber': '01 4786921',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 7:00 pm',
+  'image': 'assets/images/veg3.jpg',
+  'restaurant': 'assets/images/madre-natura.jpg'
+
+},
+{
+  'name': 'el taco naco',
+  'type': 'mexicana',
+  'district': 'Santiago de Surco',
+  'address': 'Velasco Astete 2012',
+  'price': '$',
+  'phonenumber': '01 4836521',
+  'businesshours': 'Lunes a Domingo: 2:00 pm - 12:00 am',
+  'image': 'assets/images/mex1.jpg',
+  'restaurant': 'assets/images/taco-naco.jpg'
+
+},
+{
+  'name': 'chulos',
+  'type': 'mexicana',
+  'district': 'Lince',
+  'address': 'Av.Arequipa 2394',
+  'price': '$$',
+  'phonenumber': '01 4789721',
+  'businesshours': 'Lunes a Domingo: 2:00 pm - 12:00 am',
+  'image': 'assets/images/mex2.jpg',
+  'restaurant': 'assets/images/chulos.jpg'
+
+},
+{
+  'name': 'la burrería',
+  'type': 'Mexicana',
+  'district': 'Santiago de Surco',
+  'address': 'Av. Caminos del Inca 236',
+  'price': '$$',
+  'phonenumber': '01 8772521',
+  'businesshours': 'Lunes a Domingo: 1:00 pm - 12:00 am',
+  'image': 'assets/images/mex3.jpg',
+  'restaurant': 'assets/images/bureria.jpg'
+},
+{
+  'name': 'royal',
+  'type': 'chifa',
+  'district': 'San Isidro',
+  'address': 'Av.Guillermo Prescott 231',
+  'price': '$$$',
+  'phonenumber': '01 4772521',
+  'businesshours': 'Lunes a Domingo: 1:00 pm - 11:00 pm',
+  'image': 'assets/images/chifa1.jpg',
+  'restaurant': 'assets/images/royal.jpg'
+},
+{
+  'name': 'chifa jin',
+  'type': 'chifa',
+  'district': 'Miraflores',
+  'address': 'Calle Moore 152',
+  'price': '$',
+  'phonenumber': '01 8972521',
+  'businesshours': 'Lunes a Domingo: 4:00 pm - 2:00 am',
+  'image': 'assets/images/chifa2.jpg',
+  'restaurant': 'assets/images/chifajin.jpg'
+},
+{
+  'name': 'chifa nuevo mundo',
+  'type': 'chifa',
+  'district': 'Santiago de Surco',
+  'address': 'Av.Benavides 3759',
+  'price': '$',
+  'phonenumber': '01 4772521',
+  'businesshours': 'Lunes a Domingo: 2:00 pm - 12:00 am',
+  'image': 'assets/images/chifa3.jpg',
+  'restaurant': 'assets/images/nuevomundo.jpg'
+},
+{
+  'name': 'massala',
+  'type': 'hindú',
+  'district': 'Miraflores',
+  'address': 'Calle Bolognesi 201',
+  'price': '$$',
+  'phonenumber': '01 6982521',
+  'businesshours': 'Lunes a Domingo: 2:00 pm - 9:00 pm',
+  'image': 'assets/images/hindu1.jpg',
+  'restaurant': 'assets/images/massala.jpg'
+},
+{
+  'name': 'mantra indian cuisine',
+  'type': 'hindú',
+  'district': 'Miraflores',
+  'address': 'Av.Benavides 1761',
+  'price': '$$$',
+  'phonenumber': '01 8952521',
+  'businesshours': 'Lunes a Domingo: 2:00 pm - 8:00 pm',
+  'image': 'assets/images/hindu2.jpg',
+  'restaurant': 'assets/images/mantraindian.jpg'
+},
+{
+  'name': 'mantra garden restobar',
+  'type': 'hindú',
+  'district': 'Miraflores',
+  'address': 'Calle Tarata 260',
+  'price': '$$',
+  'phonenumber': '01 4785421',
+  'businesshours': 'Lunes a Domingo: 2:00 pm - 10:00 pm',
+  'image': 'assets/images/hindu3.jpg',
+  'restaurant': 'assets/images/mantragarden.jpg'
+},
+{
+  'name': 'alameda',
+  'type': 'mediterránea',
+  'district': 'Lince',
+  'address': 'Jr.Velarde 123',
+  'price': '$$$',
+  'phonenumber': '01 4852521',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 11:00 pm',
+  'image': 'assets/images/med1.jpg',
+  'restaurant': 'assets/images/alameda.jpg'
+},
+{
+  'name': 'valentino',
+  'type': 'mediterránea',
+  'district': 'San Isidro',
+  'address': 'Calle Manuel Bañón 215',
+  'price': '$$',
+  'phonenumber': '01 4852521',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 12:00 am',
+  'image': 'assets/images/med2.jpg',
+  'restaurant': 'assets/images/valentino.jpg'
+},
+{
+  'name': 'terra mia',
+  'type': 'mediterránea',
+  'district': 'Barranco',
+  'address': 'Av.Pedra de Osma 310',
+  'price': '$$',
+  'phonenumber': '01 4771471',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 12:00 am',
+  'image': 'assets/images/med3.jpg',
+  'restaurant': 'assets/images/terramia.jpg'
+},
+{
+  'name': 'aromas peruanos',
+  'type': 'fusión',
+  'district': 'San Isidro',
+  'address': 'Av.Guardia Civil 856',
+  'price': '$$$',
+  'phonenumber': '01 4772521',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 7:00 pm',
+  'image': 'assets/images/fus1.jpg',
+  'restaurant': 'assets/images/aromasperuanos.jpg'
+},
+{
+  'name': 'la trastienda',
+  'type': 'fusión',
+  'district': 'Barranco',
+  'address': 'Av.Costa Verde S/N',
+  'price': '$$$',
+  'phonenumber': '01 4778521',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 11:00 pm',
+  'image': 'assets/images/fus2.jpg',
+  'restaurant': 'assets/images/trastienda.jpg'
+},
+{
+  'name': 'mishkina',
+  'type': 'fusión',
+  'district': 'Santiago de Surco',
+  'address': 'Av.República de Panamá 6553',
+  'price': '$$',
+  'phonenumber': '01 4772589',
+  'businesshours': 'Lunes a Domingo: 12:00 pm - 10:00 pm',
+  'image': 'assets/images/fus3.jpg',
+  'restaurant': 'assets/images/mishkina.jpg'
+}
+];
