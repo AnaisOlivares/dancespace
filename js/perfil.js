0350f11aafc37c31842b111c6c7af5cbd00118e8
AlejandraCP <alejandracp710@gmail.com>
2018-01-10 11:06:18 -0500

    Agregando funcionalidad de post

--- js/perfil.js
+++ js/perfil.js
@@ -1,1 +1,64 @@
+$(document).ready(function(){
 
+  var btn = $('#btn');
+  btn.disabled = true;
+  var textarea = $('#textarea');
+
+  textarea.keyup(function () {
+    validation();
+  });
+
+  function validation() {
+    var long = textarea.val().length;
+    var onlyText = textarea.val().replace(/\s/g, '');
+    if (long == 0 || onlyText == 0) {
+      btn.disabled = true;
+    }
+    else {
+      btn.disabled = false;
+    }
+  }
+  btn.click(function () {
+    addTask();
+    time();
+    textarea.val('');
+    btn.disabled = true;
+  });
+  function addTask() {
+    var taskContent = textarea.val();
+    var containerPost = $('#container-post');
+    var newTask = $('<div>');
+    newTask.addClass('new');
+    containerPost.append(newTask);
+    var label = $('<label>');
+    var check = $('<input>');
+    newTask.append(label);
+    var text = $(document.createTextNode(taskContent));
+    label.append(text).append('  ' + time());
+    var icon = $('<i>');
+    icon.addClass('glyphicon glyphicon-trash icon');
+    newTask.append(icon);
+
+    textarea.val('')
+
+    check.click(function () {
+      labeledTask();
+    });
+    function labeledTask() {
+      label.toggleClass('labeled');
+    }
+    icon.click(function () {
+      deleteTask();
+    });
+
+    function deleteTask() {
+      newTask.remove()
+    }
+  }
+  
+  function time() {
+    var actualDate = new Date();
+    var actualHour = actualDate.getHours() + ':' + actualDate.getMinutes();
+    return actualHour;
+  }
+})
