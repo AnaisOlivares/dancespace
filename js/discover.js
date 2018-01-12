6113e46a3f7e86b3115da3190266bf329719cfc9
AlejandraCP <alejandracp710@gmail.com>
2018-01-11 22:44:23 -0500

    Agregando funcionalidad a input

--- js/discover.js
+++ js/discover.js
@@ -1,4 +1,4 @@
-//
+
 $(function () {
   var groups = [
     'be your self',
@@ -14,25 +14,23 @@ $(function () {
 $(document).ready(function(){
   var $boxPages = $('#box-pages');
   var $inputGroups = $('#inputGroups');
-  var $pages = $('#pages');
   
-  function totalPages(e) {
-    for (i = 0; i < data1.length; i++) {
-      $boxPages.append('<div class="col-xs-10 col-md-6 col-md-offset-2 pages square-global" id="pages" data-name="' + data1[i].name + '"><h1 class="name-page">' + data1[i].name + '</h1><img class="img-pages img-responsive"  src="' + data1[i].image + '"><div><p>'+ data1[i].type + '</div></div>')
-    }
-  } 
+  for (i = 0; i < data1.length; i++) {
+    $boxPages.append('<div class="col-xs-10 col-md-6 col-md-offset-2 pages square-global" id="pages" data-name="' + data1[i].name + '"><h1 class="name-page">' + data1[i].name + '</h1><img class="img-pages img-responsive"  src="' + data1[i].image + '"><div><p>'+ data1[i].type + '</div></div>')
+  }
+  
+  var $pages = $('.pages');
+  $pages.hide();
 
-  totalPages();
-  
-  
   function showSearch() {
     var userSearch = $inputGroups.val().toLowerCase();
-    $pages.hide();
     $pages.each(function () {
       var nameGroup = $(this).data('name');
-      if (nameGroup.indexOf(userSearch) !== -1) {
+      if (userSearch === nameGroup) {
         $(this).show();
         console('son iguales');
+      } else {
+        $(this).hide();
       }
     });
   };
